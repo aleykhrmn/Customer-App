0a5fed22c0541c705c73f12bcf26e243111cacc2
Aleyna KAHRAMAN <72937626+aleykhrmn@users.noreply.github.com>
2024-10-26 11:29:48 +0300

    Add project code

---
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"; 
+import "./CustomerForm.css"
+
+const CustomerForm = ({addNewCustomer}) => {
+    const [customerName, setCustomerName] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newCustomer = {
+            id: Math.random(),
+            customerName,
+        }
+        addNewCustomer(customerName);
+        setCustomerName("");
+    }
+    return (
+        <form className='customer-form' onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        className='customer-input'
+        placeholder='Add a new customer'
+        onChange={(e) => setCustomerName(e.target.value)}
+        value={customerName}
+        />
+        <button>
+            <i className="bi bi-plus-lg"></i>
+        </button>
+        </form>
+    )
+};
+
+export default CustomerForm;
---
+++ src/components/CustomerList/CustomerList.jsx
@@ -0,0 +1,23 @@
+import CustomerItem from "../CustomerItem/CustomerItem";
+import "./CustomerList.css"
+import { useState } from "react";
+
+
+function CustomerList({customers, setCustomers}) {
+    const handleDelete = (item) => {
+        setCustomers(customers.filter((customer) => customer.id !== item.id));
+    };
+
+    return (
+    <ul className="customer-list">
+        {customers.map((customer) => (
+            <CustomerItem 
+                customer={customer} 
+                key={customer.id} 
+                handleDelete={handleDelete}/>
+        ))}
+    </ul>
+    );
+}
+
+export default CustomerList;
